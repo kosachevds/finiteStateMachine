797c27c4673ee8ba1bd441ab038d30508446c14b
Daniil Kosachev <kos-dan@mail.ru>
2019-05-05 15:41:11 +0300

    Remade with structure 'allStatesSet'

--- machine/machinestate.go
+++ machine/machinestate.go
@@ -10,20 +10,36 @@ type machineState struct {
 	ways map[rune]*machineState
 }
 
-func (ms *machineState) isFinal() bool {
-	return len(ms.ways) == 0
+type allStatesSet struct {
+	states map[int]*machineState
 }
 
 func newMachineState(code int) *machineState {
 	return &machineState{code, make(map[rune]*machineState)}
 }
 
+func (ms *machineState) isFinal() bool {
+	return len(ms.ways) == 0
+}
+
+func newStatesSet() *allStatesSet {
+	return &allStatesSet{states: make(map[int]*machineState)}
+}
+
+func (all *allStatesSet) get(stateCode int) *machineState {
+	state, ok := all.states[stateCode]
+	if !ok {
+		state = newMachineState(stateCode)
+		all.states[stateCode] = state
+	}
+	return state
+}
+
 func createStatesGraph(rules []transitionRule) (*machineState, error) {
-	// Fix: this function
 	sort.Slice(rules, func(i, j int) bool {
 		return rules[i].beginState < rules[j].beginState
 	})
-	addedStates := make(map[int]*machineState)
+	addedStates := newStatesSet()
 	for i := 0; i < len(rules); i++ {
 		stateCode := rules[i].beginState
 		j := i + 1
@@ -32,24 +48,16 @@ func createStatesGraph(rules []transitionRule) (*machineState, error) {
 				break
 			}
 		}
-		state, ok := addedStates[stateCode]
-		if !ok {
-			state = newMachineState(stateCode)
-			addedStates[stateCode] = state
-		}
+		state := addedStates.get(stateCode)
 		for _, rule := range rules[i:j] {
 			nextStateCode := rule.nextState
-			nextState, ok := addedStates[nextStateCode]
-			if !ok {
-				nextState = newMachineState(nextStateCode)
-				addedStates[rule.nextState] = nextState
-			}
-			if _, ok = state.ways[rule.symbol]; ok {
+			nextState := addedStates.get(nextStateCode)
+			if _, ok := state.ways[rule.symbol]; ok {
 				return nil, fmt.Errorf("transition rules are not deterministic")
 			}
 			state.ways[rule.symbol] = nextState
 		}
 		i = j
 	}
-	return addedStates[rules[0].beginState], nil
+	return addedStates.get(rules[0].beginState), nil
 }
